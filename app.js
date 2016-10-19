4275965ed6b12ba4d75593bfc63e80142deb222a
Lilia <geertszelia@gmail.com>
2016-10-19 15:16:28 +0200

    Supression de la v1 et refont de la v2 en design patern

--- app.js
+++ app.js
@@ -1,58 +1,62 @@
-/*
-var $input = $('input');
-console.log($input);
-$input.css('color','blue');
-*/
+(function(){
+	'use strict';
+	var app = {
+		nombre1: null,
+		nombre2: null,
+		
+		init: function(){
 
-//récupérer le contenu de input 1
-var in1 = $("#input1").val();
-	console.log("1");
+		},
 
-//récupérer le contenu de input 2
-var in2 = $("#input2").val();
-	console.log("2");
+		listeners: function(){
+			$("button").on("click");
+			$("#result").on("click");
+			$("#reset").on("click");
+		},
 
-//récupérer le contenu de select option
-var val1 = $("add").val();
-console.log("L'addition est" + " " + val1);
+		chiffre: function(){
 
-var val2 = $("substract").val();
-console.log("La soustraction est" + " " + val2);
+		},
 
-var val3 = $("multiply").val();
-console.log("La multiplication est" + " " + val3);
+		ope: function(){
+			addition: $("#add").val();
+			substract: $("#sub").val();
+			multiply: $("#mult").val();
+			divide: $("#div").val();
+		},
 
-var val4 = $("divide").val();
-console.log("La division est" + " " + val4);
+		add: function(a, b){
+			return a + b;
+		},
 
-//récuperer le boutton envoyer
-$("#button1").click(envoyer);
-function envoyer(){
-	console.log("calculer");
-}
+		sub: function(a, b){
+			return a - b;
+		},
 
-//ajout des fonctions calcul
-function add(in1, in2) {
-	return in1 + in2;
-}
+		mult: function(a, b){
+			return a * b;
+		},
 
-function substract(in1, in2) {
-	return in1 - in2;
-}
+		div: function(a, b){
+			return a / b;
+		},
 
-function multiply(in1, in2) {
-	return in1 * in2;
-}
+		calc: function(a, ope, b){
+			$("input").val(a + ope + b);
+			if(ope === ope["addition"]){
+				input(add(a, b));
+			} else if(ope === ope["substract"]){
+				input(sub(a, b));
+			} else if(ope === ope["multiply"]){
+				input(mult(a, b));
+			} else if(ope === ope["divide"]){
+				input(div(a, b));
+			}
+		},
 
-function divide(in1, in2) {
-	return in1 / in2;
-}
-
-//ajout de la fonction calc
-function calc() {
-	var x = process.argv[3];
-	var y = process.argv[4];
-
-	in1 = parseInt(x, 10);
-	in2 = parseInt(x, 10);
-}
+		reset: function(){
+			$("#ope").val("");
+			$("input").val("");
+		}
+	}
+})();
